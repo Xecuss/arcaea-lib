17ec1820c7b6fb8fbcb6cf41fabdf6f5219a5c0f
Xecus <imkkai@hotmail.com>
2019-09-02 22:22:30 +0800

    add login

--- src/Arcaea.interface.ts
+++ src/Arcaea.interface.ts
@@ -1,17 +1,22 @@
+export interface IArcLoginResponse{
+    success: boolean;
+    token_type: string;
+    access_token: string;
+}
 export interface IArcScore {
     rating: number;
     modifier: number;
     time_played: number;
-    health: number,
-    best_clear_type: number,
-    clear_type: number,
-    miss_count: number,
-    near_count: number,
-    perfect_count: number,
-    shiny_perfect_count: number,
-    score: number,
-    difficulty: number,
-    song_id: string
+    health: number;
+    best_clear_type: number;
+    clear_type: number;
+    miss_count: number;
+    near_count: number;
+    perfect_count: number;
+    shiny_perfect_count: number;
+    score: number;
+    difficulty: number;
+    song_id: string;
 }
 export interface IArcCharacter {
     is_uncapped: boolean;
--- src/Arcaea.ts
+++ src/Arcaea.ts
@@ -1,32 +1,47 @@
 import xs from 'xs-io';
-import { IArcAppregateResponse, IArcAddResponse, IArcRankResponse, IArcSelfRankResponse } from './Arcaea.interface';
+import { IArcAppregateResponse, IArcAddResponse, IArcRankResponse, IArcSelfRankResponse, IArcLoginResponse } from './Arcaea.interface';
 import { TokenNotFoundException } from './Arcaea.Exception';
 
-const addUrl="https://arcapi.lowiro.com/7/friend/me/add",
-      delUrl="https://arcapi.lowiro.com/7/friend/me/delete",
-      friendInfo="https://arcapi.lowiro.com/7/compose/aggregate?calls=%5B%7B%20%22endpoint%22%3A%20%22user%2Fme%22%2C%20%22id%22%3A%200%20%7D%2C%20%7B%20%22endpoint%22%3A%20%22purchase%2Fbundle%2Fpack%22%2C%20%22id%22%3A%201%20%7D%5D",
-      friendRankUrl="https://arcapi.lowiro.com/7/score/song/friend?start=0&limit=10",
-      worldRankUrl="https://arcapi.lowiro.com/7/score/song?start=0&limit=20",
-      selfRankUrl="https://arcapi.lowiro.com/7/score/song/me?start=4&limit=18";
+const loginUrl: string = 'https://arcapi.lowiro.com/7/auth/login',
+      addUrl: string = "https://arcapi.lowiro.com/7/friend/me/add",
+      delUrl: string = "https://arcapi.lowiro.com/7/friend/me/delete",
+      friendInfo: string = "https://arcapi.lowiro.com/7/compose/aggregate?calls=%5B%7B%20%22endpoint%22%3A%20%22user%2Fme%22%2C%20%22id%22%3A%200%20%7D%2C%20%7B%20%22endpoint%22%3A%20%22purchase%2Fbundle%2Fpack%22%2C%20%22id%22%3A%201%20%7D%5D",
+      friendRankUrl: string = "https://arcapi.lowiro.com/7/score/song/friend?start=0&limit=10",
+      worldRankUrl: string = "https://arcapi.lowiro.com/7/score/song?start=0&limit=20",
+      selfRankUrl : string= "https://arcapi.lowiro.com/7/score/song/me?start=4&limit=18";
 
 const header: Object = {
     "Accept-Encoding":"gzip, deflate",
-    "AppVersion":"2.3.0",
     "Content-Type":"application/x-www-form-urlencoded; charset=utf-8",
     "Accept-Language":"zh-cn",
     "Accept":"*/*",
     "Connection":"keep-alive",
     "Proxy-Connection":"keep-alive",
-    "User-Agent": "Arc-mobile/2.3.0.1 CFNetwork/811.5.4 Darwin/16.7.0"
 };
+
+function btoa(src: string): string{
+    return Buffer.from(src).toString('base64');
+}
+
+interface IArcArg{
+    token?: string;
+    deviceId?: string;
+    appVersion?: string;
+    userAgent?: string;
+}
 
 export default class Arcaea{
     private token: string;
+    private deviceId: string;
     private opt: any;
-    constructor(token?: string){
-        this.token = token || '';
+    constructor(Arg?: IArcArg){
+        let arg: IArcArg = Arg || {};
+        this.token = arg.token || '';
+        this.deviceId = arg.deviceId || '';
         let headers = Object.assign(header,{
-            Authorization: "Bearer "+this.token
+            Authorization: "Bearer "+this.token,
+            AppVersion: arg.appVersion || '2.3.0',
+            'User-Agent': arg.userAgent || "Arc-mobile/2.3.0.1 CFNetwork/811.5.4 Darwin/16.7.0"
         });
         this.opt = {
             headers: headers,
@@ -38,6 +53,29 @@ export default class Arcaea{
             return;
         }
         throw new TokenNotFoundException();
+    }
+    private createLoginAuth(name: string, pass: string): string{
+        let authStr = btoa(unescape(encodeURIComponent(`${name}:${pass}`)));
+        return `Basic ${authStr}`;
+    }
+    public async login(name: string, pass: string): Promise<string>{
+        let auth = this.createLoginAuth(name, pass),
+            loginHeaders = Object.assign({}, this.opt.headers, {
+                Authorization: auth,
+                DeviceId: this.deviceId
+            }),
+            loginOpt: any = {
+                headers: loginHeaders,
+                gzip: true
+            },
+            res: string = await xs.post(loginUrl,'grant_type=client_credentials', loginOpt),
+            data: IArcLoginResponse = JSON.parse(res);
+        if(data.success){
+            this.token = data.token_type + ' ' + data.access_token;
+            this.opt.headers.Authorization = this.token;
+            return this.token;
+        }
+        return '';
     }
     public async appregate(): Promise<IArcAppregateResponse>{
         this.checkToken();
